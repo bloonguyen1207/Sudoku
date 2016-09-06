6f5296b3aa8dd8821b8ab6b7c9398bb24c9c4619
Bloo Nguyen <bloonguyen1207@gmail.com>
2016-09-06 23:45:04 +0700

    lone rangers strategy added

--- project.py
+++ project.py
@@ -39,9 +39,13 @@ def generate_board(board):
                     if dup or value == board[j][col]:
                         dup = True
                         break
-                if dup or value in b:
-                    continue
 
+                for k in range(len(b)):
+                    if dup or value in b[k]:
+                        break
+
+                if dup:
+                    continue
                 board[row][col] = value
 
     return True
@@ -49,6 +53,7 @@ def generate_board(board):
 
 def block(x, y, board):
     b = []
+    br = []
     row_idx = []
     col_idx = []
     if x == 1:
@@ -67,7 +72,9 @@ def block(x, y, board):
 
     for r in row_idx:
         for c in col_idx:
-            b.append(board[r][c])
+            br.append(board[r][c])
+        b.append(br)
+        br = []
     return b
 
 
@@ -139,13 +146,14 @@ def get_possible_answers(position, board):
     p_block = block(position[0] / 3 + 1, position[1] / 3 + 1, board)
     not_answer = []
     possible_answers = []
-    for i in range(9):
+    for i in range(len(board)):
         if row[i] > 0:
             not_answer.append(row[i])
         if col[i] > 0:
             not_answer.append(col[i])
-        if p_block[i] > 0:
-            not_answer.append(p_block[i])
+        for j in range(3):
+            if p_block[i/3][j] > 0:
+                not_answer.append(p_block[i/3][j])
 
     for i in range(1, 10):
         if i not in not_answer:
@@ -153,29 +161,120 @@ def get_possible_answers(position, board):
 
     return possible_answers
 
+
+def solve(board):
+    missing_spots = get_missing(board)
+    old_len = len(missing_spots)
+    rcb_not_working = False
+    not_working = False
+    while missing_spots:
+        for i in range(len(missing_spots)):
+            pos_ans = get_possible_answers(missing_spots[i], board)
+            if len(pos_ans) == 1:
+                board[missing_spots[i][0]][missing_spots[i][1]] = pos_ans[0]
+            if i == len(missing_spots) - 1:
+                missing_spots = get_missing(board)
+                if len(missing_spots) == old_len:
+                    rcb_not_working = True
+                    break
+                else:
+                    old_len = len(missing_spots)
+
+        if rcb_not_working:
+            for i in range(len(missing_spots)):
+                for j in range(3):
+                    lone_rangers(j, missing_spots[i], missing_spots)
+            missing_spots = get_missing(board)
+            if len(missing_spots) == old_len:
+                not_working = True
+            else:
+                old_len = len(missing_spots)
+
+            if not_working:
+                return False
+
+    return True
+
 # start = time.clock()
 # game = create_board()
 # end = time.clock()
 # print "Time: ",
 # print end - start
 # print_board(game)
+
+
+def same_row(element, arr):
+    sr = []
+    row = element[0]
+    for i in range(len(arr)):
+        if arr[i][0] == row:
+            sr.append(arr[i])
+
+    return sr
+
+
+def same_col(element, arr):
+    sc = []
+    col = element[1]
+    for i in range(len(arr)):
+        if arr[i][1] == col:
+            sc.append(arr[i])
+
+    return sc
+
+
+def same_block(element, arr):
+    sb = []
+    br = element[0] / 3
+    bc = element[1] / 3
+    for i in range(len(arr)):
+        if arr[i][0] / 3 == br and arr[i][1] / 3 == bc:
+            sb.append(arr[i])
+
+    return sb
+
+
+def lone_rangers(t, position, arr):
+    cell_info = []
+    missing_elements = []
+    possible_answers = []
+    test = []
+    if t is 0:
+        test = same_block(position, arr)
+    elif t is 1:
+        test = same_row(position, arr)
+    elif t is 2:
+        test = same_col(position, arr)
+    for i in range(len(test)):
+        possible_answer = get_possible_answers(test[i], b)
+        possible_answers.append(possible_answer)
+
+    for i in range(len(possible_answers)):
+        for j in range(len(possible_answers[i])):
+            cell_info.append([possible_answers[i][j], test[i], 0])
+            missing_elements.append(possible_answers[i][j])
+
+    s1 = list(set(missing_elements))
+
+    for i in range(len(s1)):
+        for j in range(len(cell_info)):
+            if cell_info[j][0] == s1[i]:
+                cell_info[j][2] = missing_elements.count(s1[i])
+
+    for i in range(len(cell_info)):
+        if cell_info[i][2] == 1:
+            b[cell_info[i][1][0]][cell_info[i][1][1]] = cell_info[i][0]
+
+
 b = read_board("sudoku.txt")
 print_board(b)
-missing_elements = get_missing(b)
-print missing_elements
-
-print get_row(missing_elements[0], b)
-
-while missing_elements:
-    for i in range(len(missing_elements)):
-        pos_ans = get_possible_answers(missing_elements[i], b)
-        print pos_ans
-
-        if len(pos_ans) == 1:
-            b[missing_elements[i][0]][missing_elements[i][1]] = pos_ans[0]
-        if i == len(missing_elements) - 1:
-            missing_elements = get_missing(b)
-    print missing_elements
-    print_board(b)
+print solve(b)
+#     print "It didn't work"
+# else:
+#     print "Solved"
 
 print_board(b)
+
+missing = get_missing(b)
+print missing
+
